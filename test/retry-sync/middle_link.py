bd608f5c5930176a19b0d6f05ef284851e859084
Rodrigo <dev@brunneis.com>
2019-07-25 20:26:49 +0200

    Fix retry-sync test

--- test/retry-sync/middle_link.py
+++ test/retry-sync/middle_link.py
@@ -11,7 +11,7 @@ class MiddleLink(Link):
     def transform(self, electron):
         self.logger.log(f'Received: {electron.value}')
         assert electron.value == 'message 0'
-        self.suicide('rebooting link...', relaunch=True)
+        self.suicide('rebooting link...')
 
 
 if __name__ == "__main__":
